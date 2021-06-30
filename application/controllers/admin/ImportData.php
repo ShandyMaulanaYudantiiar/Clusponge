1cd0359cf215e7247e86686ca5e2f1df8033eee3
Andrea Santana A <andrea.santana986@gmail.com>
2021-06-30 10:49:55 +0700

    importCSV DONE
    Pake dataset.sql yg ada di DB saja, itu yg terbaru

--- application/controllers/admin/ImportData.php
+++ application/controllers/admin/ImportData.php
@@ -1,10 +1,183 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+if (!defined('BASEPATH')) exit('No direct script access allowed');
 
-class ImportData extends CI_Controller {
+class ImportData extends CI_Controller
+{
 
-	public function index()
-	{
-        $this->load->view('admin/importData');
-	}
+    function __construct()
+    {
+        parent::__construct();
+
+        // Load member model
+        $this->load->model('Importcsv');
+
+        // Load form validation library
+        $this->load->library('form_validation');
+
+        // Load file helper
+        $this->load->helper('file');
+    }
+
+    public function index()
+    {
+        $data = array();
+
+        // Get messages from the session
+        if ($this->session->userdata('success_msg')) {
+            $data['success_msg'] = $this->session->userdata('success_msg');
+            $this->session->unset_userdata('success_msg');
+        }
+        if ($this->session->userdata('error_msg')) {
+            $data['error_msg'] = $this->session->userdata('error_msg');
+            $this->session->unset_userdata('error_msg');
+        }
+
+        // Get rows
+        $data['dataset'] = $this->Importcsv->getRows();
+
+        // Load the list page view
+        $this->load->view('admin/importData', $data);
+    }
+
+    public function import()
+    {
+        $data = array();
+        $import = array();
+
+        // If import request is submitted
+        if ($this->input->post('importSubmit')) {
+            // Form field validation rules
+            $this->form_validation->set_rules('file', 'CSV file', 'callback_file_check');
+
+            // Validate submitted form data
+            if ($this->form_validation->run() == true) {
+                $insertCount = $updateCount = $rowCount = $notAddCount = 0;
+
+                // If file uploaded
+                if (is_uploaded_file($_FILES['file']['tmp_name'])) {
+                    // Load CSV reader library
+                    $this->load->library('CSVReader');
+
+                    // Parse data from CSV file
+                    $csvData = $this->csvreader->parse_csv($_FILES['file']['tmp_name']);
+
+                    // Insert/update CSV data into database
+                    if (!empty($csvData)) {
+                        foreach ($csvData as $row) {
+                            $rowCount++;
+
+                            // Prepare data for DB insertion
+                            $import = array(
+                                'NO' => $row['NO'],
+                                'SPESIES' => $row['SPESIES'],
+                                'A_CAPAS_DEL_CORTEX' => $row['A_CAPAS_DEL_CORTEX'],
+                                'A_CAPA_INTERNA_DEL_CORTEX' => $row['A_CAPA_INTERNA_DEL_CORTEX'],
+                                'A_CORTEX' => $row['A_CORTEX'],
+                                'A_CORTEX_FIBROSO' => $row['A_CORTEX_FIBROSO'],
+                                'A_CORTEX_SOLO_DE_ESPICULAS_TANGENCIALES' => $row['A_CAPA_INTERNA_DEL_CORTEX'],
+                                'A_CUERPOS_EXTRANOS_EN_EL_CORTEX' => $row['A_CUERPOS_EXTRANOS_EN_EL_CORTEX'],
+                                'A_GROSOR_DEL_CORTEX' => $row['A_GROSOR_DEL_CORTEX'],
+                                'A_HACES_DE_ESPICULAS_PRINCIPALES_EN_POMPON_EN_EL_CORTEX' => $row['A_HACES_DE_ESPICULAS_PRINCIPALES_EN_POMPON_EN_EL_CORTEX'],
+                                'A_TILOSTILOS_ADICIONALES_COANOSOMA' => $row['A_TILOSTILOS_ADICIONALES_COANOSOMA'],
+                                'B_NUMERO_DE_TIPOS_DE_MEGASCLERAS' => $row['B_NUMERO_DE_TIPOS_DE_MEGASCLERAS'],
+                                'C_TIPO_ESPICULA_PRINCIPAL_DIACTINA_TUBERCULADA' => $row['C_TIPO_ESPICULA_PRINCIPAL_DIACTINA_TUBERCULADA'],
+                                'C_TIPO_ESPICULA_PRINCIPAL_ESTILO' => $row['C_TIPO_ESPICULA_PRINCIPAL_ESTILO'],
+                                'C_TIPO_ESPICULA_PRINCIPAL_ESTILOS_2_TAMANOS' => $row['C_TIPO_ESPICULA_PRINCIPAL_ESTILOS_2_TAMANOS'],
+                                'C_TIPO_ESPICULA_PRINCIPAL_ESTILO_TILOSTILO' => $row['C_TIPO_ESPICULA_PRINCIPAL_ESTILO_TILOSTILO'],
+                                'C_TIPO_ESPICULA_PRINCIPAL_ESTRONGILOXA' => $row['C_TIPO_ESPICULA_PRINCIPAL_ESTRONGILOXA'],
+                                'C_TIPO_ESPICULA_PRINCIPAL_OXAS' => $row['C_TIPO_ESPICULA_PRINCIPAL_OXAS'],
+                                'C_TIPO_ESPICULA_PRINCIPAL_TILOSTILO' => $row['C_TIPO_ESPICULA_PRINCIPAL_TILOSTILO'],
+                                'D_ESPICULA_PRINCIPAL_ESTILO' => $row['D_ESPICULA_PRINCIPAL_ESTILO'],
+                                'D_ESPICULA_PRINCIPAL_TILOSTILO' => $row['D_ESPICULA_PRINCIPAL_TILOSTILO'],
+                                'D_FORMA_BASE_TILOSTILO_PRINCIPAL' => $row['D_FORMA_BASE_TILOSTILO_PRINCIPAL'],
+                                'E_DISPOSICION_MEGASCLERAS_ECTOSOMICAS_EN_EL_ECTOSOMA' => $row['E_DISPOSICION_MEGASCLERAS_ECTOSOMICAS_EN_EL_ECTOSOMA'],
+                                'E_FORMA_BASE_TILOSTILO_ECTOSOMICO' => $row['E_FORMA_BASE_TILOSTILO_ECTOSOMICO'],
+                                'E_FORMA_MEGASCLERA_ECTOSOMICA' => $row['E_FORMA_MEGASCLERA_ECTOSOMICA'],
+                                'E_TIPO_MEGASCLERA_ECTOSOMICA' => $row['E_TIPO_MEGASCLERA_ECTOSOMICA'],
+                                'F_TIPO_DE_EXOSTILO' => $row['F_TIPO_DE_EXOSTILO'],
+                                'G_FORMA_MEGASCLERA_INTERMEDIARIA' => $row['G_FORMA_MEGASCLERA_INTERMEDIARIA'],
+                                'G_TIPO_MEGASCLERA_INTERMEDIARIA' => $row['G_TIPO_MEGASCLERA_INTERMEDIARIA'],
+                                'H_LONGITUD_MEGASCLERAS' => $row['H_LONGITUD_MEGASCLERAS'],
+                                'I_MICROSCLERAS' => $row['I_MICROSCLERAS'],
+                                'I_TIPO_MICROSCLERA' => $row['I_TIPO_MICROSCLERA'],
+                                'J_ASTER' => $row['J_ASTER'],
+                                'J_DIAMETRO_ESFERASTER' => $row['J_DIAMETRO_ESFERASTER'],
+                                'J_TIPO_DE_ASTER' => $row['J_TIPO_DE_ASTER'],
+                                'J_TIPO_DE_DIPLASTER' => $row['J_TIPO_DE_DIPLASTER'],
+                                'J_TIPO_DE_ESFERASTER' => $row['J_TIPO_DE_ESFERASTER'],
+                                'K_FORMA_FINAL' => $row['K_FORMA_FINAL'],
+                                'L_NUMERO_DE_PAPILAS' => $row['L_NUMERO_DE_PAPILAS'],
+                                'L_PAPILAS' => $row['L_PAPILAS'],
+                                'M_COLOR' => $row['M_COLOR'],
+                                'N_SUPERFICIE' => $row['N_SUPERFICIE'],
+                                'O_DISPOSICION_ESPICULAR_ESQUELETO' => $row['O_DISPOSICION_ESPICULAR_ESQUELETO'],
+                                'P_ALOJA_CANGREJO_ERMITANO' => $row['P_ALOJA_CANGREJO_ERMITANO'],
+                                'P_PERFORANTE' => $row['P_PERFORANTE'],
+                                'P_PSEUDORAICES' => $row['P_PSEUDORAICES'],
+                                'P_SUSTRATO' => $row['P_SUSTRATO'],
+                            );
+
+                            // Check whether email already exists in the database
+                            $con = array(
+                                'where' => array(
+                                    'NO' => $row['NO']
+                                ),
+                                'returnType' => 'count'
+                            );
+                            $prevCount = $this->Importcsv->getRows($con);
+
+                            if ($prevCount > 0) {
+                                // Update member data
+                                $condition = array('NO' => $row['NO']);
+                                $update = $this->Importcsv->update($import, $condition);
+
+                                if ($update) {
+                                    $updateCount++;
+                                }
+                            } else {
+                                // Insert member data
+                                $insert = $this->Importcsv->insert($import);
+
+                                if ($insert) {
+                                    $insertCount++;
+                                }
+                            }
+                        }
+
+                        // Status message with imported data count
+                        $notAddCount = ($rowCount - ($insertCount + $updateCount));
+                        $successMsg = 'Data imported successfully. Total Rows (' . $rowCount . ') | Inserted (' . $insertCount . ') | Updated (' . $updateCount . ') | Not Inserted (' . $notAddCount . ')';
+                        $this->session->set_userdata('success_msg', $successMsg);
+                    }
+                } else {
+                    $this->session->set_userdata('error_msg', 'Error on file upload, please try again.');
+                }
+            } else {
+                $this->session->set_userdata('error_msg', 'Invalid file, please select only CSV file.');
+            }
+        }
+        redirect('admin/importData');
+    }
+
+    /*
+     * Callback function to check file value and type during validation
+     */
+    public function file_check($str)
+    {
+        $allowed_mime_types = array('text/x-comma-separated-values', 'text/comma-separated-values', 'application/octet-stream', 'application/vnd.ms-excel', 'application/x-csv', 'text/x-csv', 'text/csv', 'application/csv', 'application/excel', 'application/vnd.msexcel', 'text/plain');
+        if (isset($_FILES['file']['name']) && $_FILES['file']['name'] != "") {
+            $mime = get_mime_by_extension($_FILES['file']['name']);
+            $fileAr = explode('.', $_FILES['file']['name']);
+            $ext = end($fileAr);
+            if (($ext == 'csv') && in_array($mime, $allowed_mime_types)) {
+                return true;
+            } else {
+                $this->form_validation->set_message('file_check', 'Please select only CSV file to upload.');
+                return false;
+            }
+        } else {
+            $this->form_validation->set_message('file_check', 'Please select a CSV file to upload.');
+            return false;
+        }
+    }
 }
--- application/views/admin/importData.php
+++ application/views/admin/importData.php
@@ -23,6 +23,17 @@
 <div class="row">
     <div class="col-lg-12">
         <h1 class="page-header">Import Data</h1>
+        <?php if (!empty($success_msg)) { ?>
+            <div class="col-xs-12">
+                <div class="alert alert-success"><?php echo $success_msg; ?></div>
+            </div>
+            <?php if (!empty($error_msg)) { ?>
+                <div class="col-xs-12">
+                    <div class="alert alert-danger"><?php echo $error_msg; ?></div>
+                </div>
+            <?php } ?>
+        <?php } ?>
+
     </div>
 </div>
 <!--/.row-->
@@ -30,16 +41,23 @@
 <div class="row">
     <div class="col-lg-12">
         <div class="panel panel-default">
-            <div class="panel-heading">Forms</div>
-            <div class="panel-body">
+            <div class="panel-heading">
+                <div class="col-md-12 head">
+                    <div class="float-right">
+                        <a href="javascript:void(0);" class="btn btn-danger" onclick="formToggle('importFrm');"><i class="fa fa-plus"></i> Perbarui dataset</a>
+                    </div>
+                </div>
+            </div>
+            <div class="panel-body" id="importFrm" style="display: none;">
                 <div class="col-md-12">
-                    <form role="form">
+                    <form action="<?= site_url('admin/importData/import'); ?>" method="post" enctype="multipart/form-data">
                         <div class="form-group">
                             <label>Upload Dataset</label>
-                            <input type="file">
+                            <input type="file" name="file">
                             <p class="help-block" style="color: red;">*Upload dataset berekstensi csv.</p>
                         </div>
-                        <a href="<?= site_url('admin/admin') ?>" class="btn btn-danger btn-icon-split">
+                        <input type="submit" class="btn btn-primary" name="importSubmit" value="IMPORT">
+                        <!-- <a href="<?= site_url('admin/admin') ?>" class="btn btn-danger btn-icon-split">
                             <span class="icon text-white-50">
                                 <i class="fa fa-reply"></i>
                             </span>
@@ -50,12 +68,134 @@
                                 <i class="fa fa-plus"></i>
                             </span>
                             <span class="text">Upload</span>
-                        </button>
+                        </button> -->
                     </form>
                 </div>
             </div>
         </div>
+        <table class="table table-striped table-bordered">
+            <thead class="thead-dark">
+                <tr>
+                    <th>NO</th>
+                    <th>SPESIES</th>
+                    <th>A.CAPAS_DEL_CORTEX</th>
+                    <th>A.CAPA_INTERNA_DEL_CORTEX</th>
+                    <th>A.CORTEX</th>
+                    <th>A.CORTEX_FIBROSO</th>
+                    <th>A.CORTEX_SOLO_DE_ESPICULAS_TANGENCIALES</th>
+                    <th>A.CUERPOS_EXTRANOS_EN_EL_CORTEX</th>
+                    <th>A.GROSOR_DEL_CORTEX</th>
+                    <th>A.HACES_DE_ESPICULAS_PRINCIPALES_EN_POMPON_EN_EL_CORTEX</th>
+                    <th>A.TILOSTILOS_ADICIONALES_COANOSOMA</th>
+                    <th>B.NUMERO_DE_TIPOS_DE_MEGASCLERAS</th>
+                    <th>C.TIPO_ESPICULA_PRINCIPAL_DIACTINA_TUBERCULADA</th>
+                    <th>C.TIPO_ESPICULA_PRINCIPAL_ESTILO</th>
+                    <th>C.TIPO_ESPICULA_PRINCIPAL_ESTILOS_2_TAMANOS</th>
+                    <th>C.TIPO_ESPICULA_PRINCIPAL_ESTILO_TILOSTILO</th>
+                    <th>C.TIPO_ESPICULA_PRINCIPAL_ESTRONGILOXA</th>
+                    <th>C.TIPO_ESPICULA_PRINCIPAL_OXAS</th>
+                    <th>C.TIPO_ESPICULA_PRINCIPAL_TILOSTILO</th>
+                    <th>D.ESPICULA_PRINCIPAL_ESTILO</th>
+                    <th>D.ESPICULA_PRINCIPAL_TILOSTILO</th>
+                    <th>D.FORMA_BASE_TILOSTILO_PRINCIPAL</th>
+                    <th>E.DISPOSICION_MEGASCLERAS_ECTOSOMICAS_EN_EL_ECTOSOMA</th>
+                    <th>E.FORMA_BASE_TILOSTILO_ECTOSOMICO</th>
+                    <th>E.FORMA_MEGASCLERA_ECTOSOMICA</th>
+                    <th>E.TIPO_MEGASCLERA_ECTOSOMICA</th>
+                    <th>F.TIPO_DE_EXOSTILO</th>
+                    <th>G.FORMA_MEGASCLERA_INTERMEDIARIA</th>
+                    <th>G.TIPO_MEGASCLERA_INTERMEDIARIA</th>
+                    <th>H.LONGITUD_MEGASCLERAS</th>
+                    <th>I.MICROSCLERAS</th>
+                    <th>I.TIPO_MICROSCLERA</th>
+                    <th>J.ASTER</th>
+                    <th>J.DIAMETRO_ESFERASTER</th>
+                    <th>J.TIPO_DE_ASTER</th>
+                    <th>J.TIPO_DE_DIPLASTER</th>
+                    <th>J.TIPO_DE_ESFERASTER</th>
+                    <th>K.FORMA_FINAL</th>
+                    <th>L.NUMERO_DE_PAPILAS</th>
+                    <th>L.PAPILAS</th>
+                    <th>M.COLOR</th>
+                    <th>N.SUPERFICIE</th>
+                    <th>O.DISPOSICION_ESPICULAR_ESQUELETO</th>
+                    <th>P.ALOJA_CANGREJO_ERMITANO</th>
+                    <th>P.PERFORANTE</th>
+                    <th>P.PSEUDORAICES</th>
+                    <th>P.SUSTRATO</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php if (!empty($dataset)) {
+                    foreach ($dataset as $row) { ?>
+                        <tr>
+                            <td><?php echo $row['NO']; ?></td>
+                            <td><?php echo $row['SPESIES']; ?></td>
+                            <td><?php echo $row['A_CAPAS_DEL_CORTEX']; ?></td>
+                            <td><?php echo $row['A_CAPA_INTERNA_DEL_CORTEX']; ?></td>
+                            <td><?php echo $row['A_CORTEX']; ?></td>
+                            <td><?php echo $row['A_CORTEX_FIBROSO']; ?></td>
+                            <td><?php echo $row['A_CORTEX_SOLO_DE_ESPICULAS_TANGENCIALES']; ?></td>
+                            <td><?php echo $row['A_CUERPOS_EXTRANOS_EN_EL_CORTEX']; ?></td>
+                            <td><?php echo $row['A_GROSOR_DEL_CORTEX']; ?></td>
+                            <td><?php echo $row['A_HACES_DE_ESPICULAS_PRINCIPALES_EN_POMPON_EN_EL_CORTEX']; ?></td>
+                            <td><?php echo $row['A_TILOSTILOS_ADICIONALES_COANOSOMA']; ?></td>
+                            <td><?php echo $row['B_NUMERO_DE_TIPOS_DE_MEGASCLERAS']; ?></td>
+                            <td><?php echo $row['C_TIPO_ESPICULA_PRINCIPAL_DIACTINA_TUBERCULADA']; ?></td>
+                            <td><?php echo $row['C_TIPO_ESPICULA_PRINCIPAL_ESTILO']; ?></td>
+                            <td><?php echo $row['C_TIPO_ESPICULA_PRINCIPAL_ESTILOS_2_TAMANOS']; ?></td>
+                            <td><?php echo $row['C_TIPO_ESPICULA_PRINCIPAL_ESTILO_TILOSTILO']; ?></td>
+                            <td><?php echo $row['C_TIPO_ESPICULA_PRINCIPAL_ESTRONGILOXA']; ?></td>
+                            <td><?php echo $row['C_TIPO_ESPICULA_PRINCIPAL_OXAS']; ?></td>
+                            <td><?php echo $row['C_TIPO_ESPICULA_PRINCIPAL_TILOSTILO']; ?></td>
+                            <td><?php echo $row['D_ESPICULA_PRINCIPAL_ESTILO']; ?></td>
+                            <td><?php echo $row['D_ESPICULA_PRINCIPAL_TILOSTILO']; ?></td>
+                            <td><?php echo $row['D_FORMA_BASE_TILOSTILO_PRINCIPAL']; ?></td>
+                            <td><?php echo $row['E_DISPOSICION_MEGASCLERAS_ECTOSOMICAS_EN_EL_ECTOSOMA']; ?></td>
+                            <td><?php echo $row['E_FORMA_BASE_TILOSTILO_ECTOSOMICO']; ?></td>
+                            <td><?php echo $row['E_FORMA_MEGASCLERA_ECTOSOMICA']; ?></td>
+                            <td><?php echo $row['E_TIPO_MEGASCLERA_ECTOSOMICA']; ?></td>
+                            <td><?php echo $row['F_TIPO_DE_EXOSTILO']; ?></td>
+                            <td><?php echo $row['G_FORMA_MEGASCLERA_INTERMEDIARIA']; ?></td>
+                            <td><?php echo $row['G_TIPO_MEGASCLERA_INTERMEDIARIA']; ?></td>
+                            <td><?php echo $row['H_LONGITUD_MEGASCLERAS']; ?></td>
+                            <td><?php echo $row['I_MICROSCLERAS']; ?></td>
+                            <td><?php echo $row['I_TIPO_MICROSCLERA']; ?></td>
+                            <td><?php echo $row['J_ASTER']; ?></td>
+                            <td><?php echo $row['J_DIAMETRO_ESFERASTER']; ?></td>
+                            <td><?php echo $row['J_TIPO_DE_ASTER']; ?></td>
+                            <td><?php echo $row['J_TIPO_DE_DIPLASTER']; ?></td>
+                            <td><?php echo $row['J_TIPO_DE_ESFERASTER']; ?></td>
+                            <td><?php echo $row['K_FORMA_FINAL']; ?></td>
+                            <td><?php echo $row['L_NUMERO_DE_PAPILAS']; ?></td>
+                            <td><?php echo $row['L_PAPILAS']; ?></td>
+                            <td><?php echo $row['M_COLOR']; ?></td>
+                            <td><?php echo $row['N_SUPERFICIE']; ?></td>
+                            <td><?php echo $row['O_DISPOSICION_ESPICULAR_ESQUELETO']; ?></td>
+                            <td><?php echo $row['P_ALOJA_CANGREJO_ERMITANO']; ?></td>
+                            <td><?php echo $row['P_PERFORANTE']; ?></td>
+                            <td><?php echo $row['P_PSEUDORAICES']; ?></td>
+                            <td><?php echo $row['P_SUSTRATO']; ?></td>
+                        </tr>
+                    <?php }
+                } else { ?>
+                    <tr>
+                        <td colspan="5">No data(s) found...</td>
+                    </tr>
+                <?php } ?>
+            </tbody>
+        </table>
     </div><!-- /.panel-->
+    <script>
+        function formToggle(ID) {
+            var element = document.getElementById(ID);
+            if (element.style.display === "none") {
+                element.style.display = "block";
+            } else {
+                element.style.display = "none";
+            }
+        }
+    </script>
 </div><!-- /.col-->
 
 <!-- Footer -->
