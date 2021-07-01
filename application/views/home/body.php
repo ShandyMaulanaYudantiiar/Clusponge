318398348540b2bda0be629a03feea2d206b5a2a
Riodihan_ <minatoramakaze@gmail.com>
2021-07-01 15:06:35 +0700

    user

---
+++ application/views/home/body.php
@@ -0,0 +1,86 @@
+<!-- Wrapper -->
+<div id="wrapper">
+
+<!-- Header -->
+    <header id="header" class="alt">
+        <span class="logo"><img src="assets/user/images/logo.svg" alt="" /></span>
+        <h1>Clustering for Sponge Dataset</h1>
+        <p>Klastering dataset terumbu karang dengan metode K-means klastering<br />
+        <!-- built by <a href="https://twitter.com/ajlkn">@ajlkn</a> for <a href="https://html5up.net">HTML5 UP</a>.</p> -->
+    </header>
+
+<!-- Nav -->
+    <nav id="nav">
+        <ul>
+            <li><a href="#intro" class="active">Introduction</a></li>
+            <li><a href="#perhitungan">Perhitungan</a></li>
+            <li><a href="#tentangKami">Tentang Kami</a></li>
+        </ul>
+    </nav>
+
+<!-- Main -->
+    <div id="main">
+
+        <!-- Introduction -->
+            <section id="intro" class="main">
+                <div class="spotlight">
+                    <div class="content">
+                        <header class="major">
+                            <h2>Terumbu Karang</h2>
+                        </header>
+                        <p>A sponge is a member of the phylum Porifera. It is a simple animal with many cells, but no mouth,
+                            muscles, heart or brain. It is sessile: it cannot move from place to place the way most animals can.
+                            A sponge is an animal that grows in one spot like most plants do. Nevertheless, sponges are quite successful.</p>
+                        <!-- <ul class="actions">
+                            <li><a href="generic.html" class="button">Learn More</a></li>
+                        </ul> -->
+                    </div>
+                    <span class="image"><img src="assets/user/images/pic01.jpg" alt="" /></span>
+                </div>
+            </section>
+
+        <!-- Perhitungan -->
+            <section id="perhitungan" class="main special">
+                <header class="major">
+                    <h2>Perhitungan</h2>
+                </header>
+                <ul class="features">
+                    <li>
+                        <span class="icon solid major style1 fa-code"></span>
+                        <h3>Ipsum consequat</h3>
+                        <p>Sed lorem amet ipsum dolor et amet nullam consequat a feugiat consequat tempus veroeros sed consequat.</p>
+                    </li>
+                    <li>
+                        <span class="icon major style3 fa-copy"></span>
+                        <h3>Amed sed feugiat</h3>
+                        <p>Sed lorem amet ipsum dolor et amet nullam consequat a feugiat consequat tempus veroeros sed consequat.</p>
+                    </li>
+                    <li>
+                        <span class="icon major style5 fa-gem"></span>
+                        <h3>Dolor nullam</h3>
+                        <p>Sed lorem amet ipsum dolor et amet nullam consequat a feugiat consequat tempus veroeros sed consequat.</p>
+                    </li>
+                </ul>
+                <footer class="major">
+                    <ul class="actions special">
+                        <li><a href="generic.html" class="button">Learn More</a></li>
+                    </ul>
+                </footer>
+            </section>
+
+        <!-- Tentang Kami -->
+            <section id="tentangKami" class="main special">
+                <header class="major">
+                    <h2>Congue imperdiet</h2>
+                    <p>Donec imperdiet consequat consequat. Suspendisse feugiat congue<br />
+                    posuere. Nulla massa urna, fermentum eget quam aliquet.</p>
+                </header>
+                <footer class="major">
+                    <ul class="actions special">
+                        <li><a href="generic.html" class="button primary">Get Started</a></li>
+                        <li><a href="generic.html" class="button">Learn More</a></li>
+                    </ul>
+                </footer>
+            </section>
+
+    </div>
---
+++ application/views/home/foot.php
@@ -0,0 +1,43 @@
+<!-- Footer -->
+<footer id="footer">
+						<section>
+							<h2>Clustering for Sponge Dataset</h2>
+							<p>Sed lorem ipsum dolor sit amet et nullam consequat feugiat consequat magna adipiscing tempus etiam dolore veroeros. eget dapibus mauris. Cras aliquet, nisl ut viverra sollicitudin, ligula erat egestas velit, vitae tincidunt odio.</p>
+							<ul class="actions">
+								<!-- <li><a href="generic.html" class="button">Learn More</a></li> -->
+							</ul>
+						</section>
+						<section>
+							<h2>Kontak Kami</h2>
+							<dl class="alt">
+								<dt>Alamat</dt>
+								<dd>Politeknik Negeri Jember &bull; Jember &bull; Jawa Timur</dd>
+								<dt>Telepon</dt>
+								<dd>(62) 815-544-008-81</dd>
+								<dt>Email</dt>
+								<dd><a href="#">minatoramakaze@gmail.com</a></dd>
+							</dl>
+							<ul class="icons">
+								<li><a href="#" class="icon brands fa-twitter alt"><span class="label">Twitter</span></a></li>
+								<li><a href="#" class="icon brands fa-facebook-f alt"><span class="label">Facebook</span></a></li>
+								<li><a href="#" class="icon brands fa-instagram alt"><span class="label">Instagram</span></a></li>
+								<li><a href="#" class="icon brands fa-github alt"><span class="label">GitHub</span></a></li>
+								<li><a href="#" class="icon brands fa-dribbble alt"><span class="label">Dribbble</span></a></li>
+							</ul>
+						</section>
+						<!-- <p class="copyright">&copy; Untitled. Design: <a href="https://html5up.net">HTML5 UP</a>.</p> -->
+					</footer>
+
+			</div>
+
+		<!-- Scripts -->
+			<script src="assets/user/js/jquery.min.js"></script>
+			<script src="assets/user/js/jquery.scrollex.min.js"></script>
+			<script src="assets/user/js/jquery.scrolly.min.js"></script>
+			<script src="assets/user/js/browser.min.js"></script>
+			<script src="assets/user/js/breakpoints.min.js"></script>
+			<script src="assets/user/js/util.js"></script>
+			<script src="assets/user/js/main.js"></script>
+
+	</body>
+</html>
