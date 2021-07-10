3de69d18b71a42e83201420347675b278db3e188
Shandy Maulana Yudantiar <shandy318@gmail.com>
2021-07-10 11:03:11 +0700

    iterasi1

--- application/controllers/Perhitungan.php
+++ application/controllers/Perhitungan.php
@@ -1,6 +1,5 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
-
 class Perhitungan extends CI_Controller {
 
 	function __construct()	{
@@ -16,38 +15,126 @@ class Perhitungan extends CI_Controller {
 		$this->load->view('template/foot');
 	}
 
-	function hasil()	{
+	public function hasil()	{
+		$jumlah_centroid = $_POST['jumklas'];
+		$data = $this->KMeans->getData();		//mengambil Seluruh data
 
-		$jk = $_POST['jumklas'];						// Variabel data Jumlah Input Klastering
-		$jd = $this->KMeans->hitungBaris();				// Variabel Jumlah Baris data
-		$randClus = $this->KMeans->random($jk);			// Variabel Klaster Random
-		$data = $this->KMeans->getAll();				//Variabel Seluruh Data
-		// var_dump($data);
+		$centroid = $this->Centroid_Random($data,$jumlah_centroid);
+		$cluster = $this->clustering($data, $centroid);
+		$new_centroid = $this->newCentroid($data, $cluster, $centroid);
 		
-		foreach($data as $data)	{					//Menampilkan All Data
-			
-				foreach ($randClus as $Clus) {		//Menampilkan Data Random
-					
-					for ($i=0; $i<$jk ; $i++) { 		//Perulangan agar menciptakan variabel hasil dinamis
-						$hasil[$i] = sqrt(
-							pow($data->A_CAPAS_DEL_CORTEX - $Clus->A_CAPAS_DEL_CORTEX,2) +
-							pow($data->A_CAPA_INTERNA_DEL_CORTEX - $Clus->A_CAPA_INTERNA_DEL_CORTEX,2) +
-							pow($data->A_CORTEX - $Clus->A_CORTEX,2) +
-							pow($data->A_CORTEX_FIBROSO - $Clus->A_CORTEX_FIBROSO,2) +
-							pow($data->A_CORTEX_SOLO_DE_ESPICULAS_TANGENCIALES - $Clus->A_CORTEX_SOLO_DE_ESPICULAS_TANGENCIALES,2) +
-							pow($data->A_CUERPOS_EXTRANOS_EN_EL_CORTEX - $Clus->A_CUERPOS_EXTRANOS_EN_EL_CORTEX,2) +
-							pow($data->A_GROSOR_DEL_CORTEX - $Clus->A_GROSOR_DEL_CORTEX,2) +
-							pow($data->A_HACES_DE_ESPICULAS_PRINCIPALES_EN_POMPON_EN_EL_CORTEX - $Clus->A_HACES_DE_ESPICULAS_PRINCIPALES_EN_POMPON_EN_EL_CORTEX,2) +
-							pow($data->A_TILOSTILOS_ADICIONALES_COANOSOMA - $Clus->A_TILOSTILOS_ADICIONALES_COANOSOMA,2)
-							// pow($data-$Clus,2) + capek
-						);
-					}
-				}
+		var_dump($cluster);
+	}
 
+	public function Centroid_Random($data, $jumlah_centroid)	{
+		$index_data = count($data)-1;  //Karena Index dimulai dari 0. maka jumlah index jumlah data - 1
+		// Deklarasi Variable array
+		$centroid_random = array();	
+		$angka = array();
+		$data_centroid = array();
+
+		// Perulangan untuk memuat data Centroid Random
+		for ($i=0; $i < $jumlah_centroid ; $i++) {
+			$angka = rand( 0, $index_data);
+			// Pada perulangan ini mencari nilai random untuk index centroid random
+			// Dilakukan Validasi menggunakan perulangan do while
+			//untuk memastikan tidak ada angka yang sama
+			do {
+				$angka = rand( 0, $index_data);
+				// Jika Angka sama akan dilakukan perulangan lagi
+			} while (in_array($angka, $centroid_random));
+			// Perulangan akan terus terjadi JIKA
+			// Nilai pada array angka sama dengan Nilai array Centroid_Random
+				$centroid_random[$i] = $angka;
+				// Jika Nilai belum ada maka masuk ke $centroid_random
 		}
-		var_dump($hasil);
-		echo $data->P_SUSTRATO;
-		echo min($hasil);
+
+		// Setelah mencari index dengan angka random
+		// saatnya Mencari data berdasarkan index yang didapat
+		foreach ($centroid_random as $key_CRandom => $value_CRandom) {
+			//Untuk memisahkan index dengan Value
+			$data_centroid[$key_CRandom] = $data[$value_CRandom];
+			// Memasukkan value Centroid random ke Data Centroid
+		}
+		return $data_centroid;
+	}
+
+	public function clustering($data, $centroid){   
+		// Menghutung jarak setiap data dengan centroid
+		// lalu jarak tersebut di simpan kedalan variable $jarak_centroid.
+		foreach($data as $row_m_data => $m_data){ 
+			foreach($centroid as $row_m_centroid => $m_centroid){
+				// mendefinisikan jarak awal seelum perhitungan setiap data = 0
+				// agar tidak terjadi salah perhitungan pada looping berikutnya.
+				$jarak_centroid[$row_m_data][$row_m_centroid] = 0.0;
+				$jarak = 0.0;
+				// echo "sqrt(";
+				foreach($m_centroid as $row_single_centroid => $single_centroid){
+					$single_data = $m_data[$row_single_centroid];
+					$jarak += pow(($single_data - $single_centroid), 2);
+					// echo"(($single_data - $single_centroid)^ 2) [$jarak] + ";
+				}
+				// echo ")";
+				$hasil = round(sqrt($jarak),2);
+				$jarak_centroid[$row_m_data][$row_m_centroid] = $hasil;
+				// echo "= " . $hasil. "<br>";
+			}
+			// echo"<br><br>";
+		}
+
+		// Setelah melakukan perhitungan jarak, maka ditentukan lah jarak terdekat dari - 
+		// setiap data dengan setiap centroid.
+		// jarak rata rata data dari setiap nilai siswa yang paling dekat dengan centroid -
+		// yang ada akan di simpan ke dalam var new_centroid lalu di kembalikan kedalam
+		// variable yang memanggil fungsi clustering().
+		foreach($jarak_centroid as $m_jarak){ // Menghitung jarak terdekat
+			$nearest_cluster[] = array_search(min($m_jarak), $m_jarak);
+		}
+
+		// echo "<br><br>";
+		// echo"new centroid<br>";
+		// print_r(json_encode($new_centroid));
+		// echo "<br><br>";
+
+		return $nearest_cluster;
+	}
+
+	// fungsi untuk menentukan titik centroid terbaru.
+	function newCentroid($data, $cluster, $centroid){
+		// setiap data yang memiliki cluster yang sama akan di masukkan ke dalam $multi_cluster -
+		// sesuai index nya masing masing, index disini menggantikan nilai cluster yang di tuju -
+		// oleh setiap data.
+		$multi_cluster=array();
+		foreach($cluster as $row_m_cluster=>$m_cluster){
+			$multi_cluster[$m_cluster][] = $row_m_cluster;
+		}
+		
+		// pada pemrosesan sebelumnya dapat terlihat bahwa data yang di simpan akan berurutkan -
+		// cluster berapa yag di miliki oleh data pertama, maka dari itu data tersebut harus -
+		// di urutkan terlebih dahulu menggunakan fungsi ksort(),
+		// ksort() = mengurutkan secara ascending berdasarkan nilai key yang dimiliki.
+		ksort($multi_cluster);
+		
+		// proses dibawah berfungsi untuk menghitung centroid baru sesuai dengan -
+		// metode k-means, lalu nilai dari centroid baru disimpan kedalam var $centroid_baru
+		// di kembalikan kedalam variable yang memanggil fungsi new_centroid()
+		$centroid_baru = array();
+		foreach($multi_cluster as $row_m_multi_cluster=>$m_multi_cluster){
+			foreach($centroid[0] as $row_m_centroid=>$_m_centroid){ 
+				$temp_centroid = 0;
+				// echo "<br>start||";
+				foreach($m_multi_cluster as $n_multi_cluster){
+					$temp_centroid += $data[$n_multi_cluster][$row_m_centroid];
+					// $original = $data[$n_multi_cluster][$row_m_centroid];
+					// echo "[$n_multi_cluster][$row_m_centroid]=$original +   ";
+				}
+				$temp_centroid = round($temp_centroid/count($m_multi_cluster), 2);
+				$centroid_baru[$row_m_multi_cluster][] = $temp_centroid;
+				// echo " / ". count($m_multi_cluster) . "= ". $temp_centroid ;
+			}
+		}
+		// echo "<br>";
+		return $centroid_baru;
 	}
 }
 
--- application/models/kmeans.php
+++ application/models/kmeans.php
@@ -10,14 +10,110 @@ class KMeans extends CI_Model
 
     // DATA RANDOM
     public function random($jk)    {
-        $this->db->order_by('rand()');
-        $this->db->limit($jk);
-        return $this->db->get('dataset')->result();
+        return $this->db->query("SELECT A_CAPAS_DEL_CORTEX,
+                                        A_CAPA_INTERNA_DEL_CORTEX,
+                                        A_CORTEX,
+                                        A_CORTEX_FIBROSO,
+                                        A_CORTEX_SOLO_DE_ESPICULAS_TANGENCIALES,
+                                        A_CUERPOS_EXTRANOS_EN_EL_CORTEX,
+                                        A_GROSOR_DEL_CORTEX,
+                                        A_HACES_DE_ESPICULAS_PRINCIPALES_EN_POMPON_EN_EL_CORTEX,
+                                        A_TILOSTILOS_ADICIONALES_COANOSOMA,
+                                        B_NUMERO_DE_TIPOS_DE_MEGASCLERAS,
+                                        C_TIPO_ESPICULA_PRINCIPAL_DIACTINA_TUBERCULADA,
+                                        C_TIPO_ESPICULA_PRINCIPAL_ESTILO,
+                                        C_TIPO_ESPICULA_PRINCIPAL_ESTILOS_2_TAMANOS,
+                                        C_TIPO_ESPICULA_PRINCIPAL_ESTILO_TILOSTILO,
+                                        C_TIPO_ESPICULA_PRINCIPAL_ESTRONGILOXA,
+                                        C_TIPO_ESPICULA_PRINCIPAL_OXAS,
+                                        C_TIPO_ESPICULA_PRINCIPAL_TILOSTILO,
+                                        D_ESPICULA_PRINCIPAL_ESTILO,
+                                        D_ESPICULA_PRINCIPAL_TILOSTILO,
+                                        D_FORMA_BASE_TILOSTILO_PRINCIPAL,
+                                        E_DISPOSICION_MEGASCLERAS_ECTOSOMICAS_EN_EL_ECTOSOMA,
+                                        E_FORMA_BASE_TILOSTILO_ECTOSOMICO,
+                                        E_FORMA_MEGASCLERA_ECTOSOMICA,
+                                        E_TIPO_MEGASCLERA_ECTOSOMICA,
+                                        F_TIPO_DE_EXOSTILO,
+                                        G_FORMA_MEGASCLERA_INTERMEDIARIA,
+                                        H_LONGITUD_MEGASCLERAS,
+                                        I_MICROSCLERAS,
+                                        I_TIPO_MICROSCLERA,
+                                        J_ASTER,
+                                        J_DIAMETRO_ESFERASTER,
+                                        J_TIPO_DE_ASTER,
+                                        J_TIPO_DE_DIPLASTER,
+                                        J_TIPO_DE_ESFERASTER,
+                                        K_FORMA_FINAL,
+                                        L_NUMERO_DE_PAPILAS,
+                                        L_PAPILAS,
+                                        M_COLOR,
+                                        N_SUPERFICIE,
+                                        O_DISPOSICION_ESPICULAR_ESQUELETO,
+                                        P_ALOJA_CANGREJO_ERMITANO,
+                                        P_PERFORANTE,
+                                        P_PSEUDORAICES,
+                                        P_SUSTRATO
+                                FROM dataset")->result_array();
     }
 
     // GET DATA dari Model
     function getAll()   {
-        return $this->db->query("SELECT * FROM dataset")->result();
+        return $this->db->query("SELECT * FROM dataset")->result_array();
+    }
+
+    // GET DATA dari Model
+    function getData()   {
+        return $this->db->query("SELECT A_CAPAS_DEL_CORTEX,
+                                        A_CAPA_INTERNA_DEL_CORTEX,
+                                        A_CORTEX,
+                                        A_CORTEX_FIBROSO,
+                                        A_CORTEX_SOLO_DE_ESPICULAS_TANGENCIALES,
+                                        A_CUERPOS_EXTRANOS_EN_EL_CORTEX,
+                                        A_GROSOR_DEL_CORTEX,
+                                        A_HACES_DE_ESPICULAS_PRINCIPALES_EN_POMPON_EN_EL_CORTEX,
+                                        A_TILOSTILOS_ADICIONALES_COANOSOMA,
+                                        B_NUMERO_DE_TIPOS_DE_MEGASCLERAS,
+                                        C_TIPO_ESPICULA_PRINCIPAL_DIACTINA_TUBERCULADA,
+                                        C_TIPO_ESPICULA_PRINCIPAL_ESTILO,
+                                        C_TIPO_ESPICULA_PRINCIPAL_ESTILOS_2_TAMANOS,
+                                        C_TIPO_ESPICULA_PRINCIPAL_ESTILO_TILOSTILO,
+                                        C_TIPO_ESPICULA_PRINCIPAL_ESTRONGILOXA,
+                                        C_TIPO_ESPICULA_PRINCIPAL_OXAS,
+                                        C_TIPO_ESPICULA_PRINCIPAL_TILOSTILO,
+                                        D_ESPICULA_PRINCIPAL_ESTILO,
+                                        D_ESPICULA_PRINCIPAL_TILOSTILO,
+                                        D_FORMA_BASE_TILOSTILO_PRINCIPAL,
+                                        E_DISPOSICION_MEGASCLERAS_ECTOSOMICAS_EN_EL_ECTOSOMA,
+                                        E_FORMA_BASE_TILOSTILO_ECTOSOMICO,
+                                        E_FORMA_MEGASCLERA_ECTOSOMICA,
+                                        E_TIPO_MEGASCLERA_ECTOSOMICA,
+                                        F_TIPO_DE_EXOSTILO,
+                                        G_FORMA_MEGASCLERA_INTERMEDIARIA,
+                                        H_LONGITUD_MEGASCLERAS,
+                                        I_MICROSCLERAS,
+                                        I_TIPO_MICROSCLERA,
+                                        J_ASTER,
+                                        J_DIAMETRO_ESFERASTER,
+                                        J_TIPO_DE_ASTER,
+                                        J_TIPO_DE_DIPLASTER,
+                                        J_TIPO_DE_ESFERASTER,
+                                        K_FORMA_FINAL,
+                                        L_NUMERO_DE_PAPILAS,
+                                        L_PAPILAS,
+                                        M_COLOR,
+                                        N_SUPERFICIE,
+                                        O_DISPOSICION_ESPICULAR_ESQUELETO,
+                                        P_ALOJA_CANGREJO_ERMITANO,
+                                        P_PERFORANTE,
+                                        P_PSEUDORAICES,
+                                        P_SUSTRATO
+                                FROM dataset")->result_array();
+    }
+
+    // GET DATA dari Model
+    function getNama()   {
+        return $this->db->query("SELECT SPESIES FROM dataset")->result_array();
     }
 
     // GET Jumlah Row dataset
