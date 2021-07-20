8d1623c070e3fde6b842632780816d0c8f0f89e7
Shandy Maulana Yudantiar <shandy318@gmail.com>
2021-07-20 18:56:19 +0700

    perhitungan done fix no debat

--- application/controllers/hasil.php
+++ application/controllers/hasil.php
@@ -19,19 +19,41 @@ class hasil extends CI_Controller {
 		$cluster = $this->clustering($data, $centroid);
 		$new_centroid = $this->newCentroid($data, $cluster, $centroid);
 
+		$iterasi = 1;
+		// Proses pengclusteran dan penentuan cetntroid baru akan di ulang sampai tidak -
+		// ada perubahan titik pada centroid baru dengan centroid sebelumnya
+		while($new_centroid!=$centroid){
+			$iterasi++;
+			$centroid = $new_centroid;
+			$cluster = $this->clustering($data, $centroid);
+			$new_centroid = $this->newCentroid($data, $cluster, $centroid);
+		}
+		// var_dump($centroid);
+		// var_dump($cluster2);
+		
+		
+		$multi_cluster=array();
+		foreach($cluster as $row_m_cluster=>$m_cluster){
+			$multi_cluster[$m_cluster][] = $row_m_cluster;
+		}
+		// var_dump($multi_cluster);
+
 		$hasil = array();
 		$hasil['jumlah_centroid'] = $jumlah_centroid;
 		$hasil['jumlah_data'] = $this->KMeans->hitungBaris();
+		$hasil['jumlah_iterasi'] = $iterasi;
 		$hasil['centroid'] = $centroid;
 		$hasil['cluster'] = $cluster;
+		$hasil['multi_cluster'] = $multi_cluster;
 		$hasil['Spesies'] = $this->KMeans->getNama();
+		// var_dump($hasil['Spesies']);
 
 		$this->load->view('template/head');
 		$this->load->view('user/hasil', $hasil);
 		$this->load->view('template/foot');
 	}
 
-    public function Centroid_Random($data, $jumlah_centroid)	{
+	public function Centroid_Random($data, $jumlah_centroid)	{
 		$index_data = count($data)-1;  //Karena Index dimulai dari 0. maka jumlah index jumlah data - 1
 		// Deklarasi Variable array
 		$centroid_random = array();	
@@ -61,6 +83,7 @@ class hasil extends CI_Controller {
 			$data_centroid[$key_CRandom] = $data[$value_CRandom];
 			// Memasukkan value Centroid random ke Data Centroid
 		}
+		
 		return $data_centroid;
 	}
 
@@ -118,7 +141,7 @@ class hasil extends CI_Controller {
 		// di kembalikan kedalam variable yang memanggil fungsi new_centroid()
 		$centroid_baru = array();
 		foreach($multi_cluster as $row_m_multi_cluster=>$m_multi_cluster){
-			foreach($centroid[0] as $row_m_centroid=>$_m_centroid){ 
+			foreach($centroid[0] as $row_m_centroid=>$_m_centroid){ // hanya agar looping sebanyak jumlah mapel :D
 				$temp_centroid = 0;
 				// echo "<br>start||";
 				foreach($m_multi_cluster as $n_multi_cluster){
@@ -127,11 +150,8 @@ class hasil extends CI_Controller {
 					// echo "[$n_multi_cluster][$row_m_centroid]=$original +   ";
 				}
 				$temp_centroid = round($temp_centroid/count($m_multi_cluster), 2);
-				$centroid_baru[$row_m_multi_cluster][] = $temp_centroid;
-				// echo " / ". count($m_multi_cluster) . "= ". $temp_centroid ;
+				$centroid_baru[$row_m_multi_cluster][$row_m_centroid] = $temp_centroid;
 			}
-		}
-		// echo "<br>";
-		return $centroid_baru;
+    }return $centroid_baru;
 	}
 }
--- application/views/user/hasil.php
+++ application/views/user/hasil.php
@@ -1,44 +1,65 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>K-Mean Clustering - All cluster protoFCM</title>
-        <style type='text/css'>
-        * {font-family:verdana,arial,sans-serif;font-size:12pt;}
-        h1{font-size:18pt;}
-        h2{font-size:14pt;line-height:16pt;}
-        fieldset{margin:5px;padding:5px;background-color:#f5f5f5;}
-        legend {font-weight:bold;padding:5px;background-color:#0084b4;color:#ffffff;}
-        .inptxt{text-align:right;}
-        .member{background-color:#cc3333;font-weight:bold;color:#ffffff;}
-        </style>
-    </head>
-    <body>
-        <div id='container'>
-        <fieldset>
-            <legend>Parameter</legend>
-            Jumlah Cluster =<?php echo $jumlah_centroid; ?><br>
-            Jumlah iterasi =<?php echo $jumlah_iterasi; ?><br>
-        </fieldset>
-        <fieldset>
-            <legend>Hasil</legend>
-            <table border='1'>
-            <thead>
-                <tr>
-                    <th>Centroid</th>
-                    <th>Anggota</th>
-                </tr>
-            </thead>
-            <tbody>
-                <?php for ($i=0; $i < count($Spesies) ; $i++) { ?>
-                <tr>
-                    <?php echo "<td>$Spesies[$i]</td>";?>
-                    <td>oi</td>
-                </tr>
-            <?php }?>
-            </tbody>
-            
-            </table>
-        </fieldset>
+<!-- Wrapper -->
+<div id="wrapper">
+    <style type='text/css'>
+      * {font-family:verdana,arial,sans-serif;font-size:12pt;}
+      h1{font-size:18pt;}
+      h2{font-size:14pt;line-height:16pt;}
+      fieldset{margin:5px;padding:5px;background-color:#f5f5f5;}
+      legend {font-weight:bold;padding:5px;background-color:#0084b4;color:#ffffff;}
+      .inptxt{text-align:right;}
+      .member{background-color:#cc3333;font-weight:bold;color:#ffffff;}
+    </style>
+
+    <!-- Header -->
+    <header id="header">
+        <h1>Hasil Clustering K-Means</h1>
+    </header>
+
+    <!-- Main -->
+    <div id="main">
+
+        <!-- Content -->
+        <section id="content" class="main">
+            <div class="col-md">
+
+                <!-- Form -->
+                <section>
+                    <fieldset>
+                        <legend>Parameter</legend>
+                        Jumlah Cluster  =<?php echo $jumlah_centroid;?><br>
+                        Jumlah iterasi =<?php echo $jumlah_iterasi; ?><br>
+                    </fieldset>
+                    <fieldset>
+                        <legend>
+                            Hasil
+                        </legend>
+                        <table border = "1">
+                            <tr>
+                                <th>Cluster</th>
+                                <th>Anggota</th>
+                            </tr>
+                                <?php for ($i=0; $i < $jumlah_centroid; $i++) { ?>
+                                <tr>
+                                    <td align="center">
+                                        <b><?php echo $i+1 ;?></b>
+                                    </td>
+                                            <td>
+                                                <b><?php 
+                                                for ($j=0; $j < count($multi_cluster[$i]) ; $j++) { 
+                                                    $k = $multi_cluster[$i][$j];
+                                                    echo implode($Spesies[$k]);
+                                                    echo " | ";
+                                                }?></b>
+                                            </td>
+                                </tr>
+                                <?php }?>
+                        </table>
+                    </fieldset>
+                </section>
 
 
-</div>
+            </div>
+        </section>
+
+
+    </div>
